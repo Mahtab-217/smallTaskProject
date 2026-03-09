9b6f18b8f7144f0d996bc215615fff1e1a1e0113
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-09 21:15:48 +0430

    exporting data

---
+++ light.js
@@ -0,0 +1,22 @@
+const list=[
+    {
+    id: 1,
+    name: "Mahtab",
+    fatherName:"Ali",
+    }
+    {
+    id: 2,
+    name: "Frishta",
+    fatherName:"Ali Reza",
+    }
+    {
+    id: 3,
+    name: "Laila",
+    fatherName:"Ali Ahmad",
+    }
+    {
+    id: 4,
+    name: "Mahdi",
+    fatherName:"Muhammad",
+    }
+]
